f9ed19d624dd5c8e0d967bf496015d2ec6198834
Shreyas Ananda Poojary <164512022+poojarishreyas@users.noreply.github.com>
2026-09-11 23:34:40 +0530

    Time: 19 ms (34.79%) | Memory: 50.6 MB (85.10%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,43 +1,22 @@
 class Solution {
-    class Pair {
-        int value;
-        int row;
-        int col;
-
-        Pair(int value, int row, int col) {
-            this.value = value;
-            this.row = row;
-            this.col = col;
-        }
-    }
-
     public int kthSmallest(int[][] matrix, int k) {
+        int n=0;
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
 
-        int n = matrix.length;
-
-        PriorityQueue<Pair> pq =
-            new PriorityQueue<>((a, b) -> a.value - b.value);
-
-        // Put first element of every row
-        for (int i = 0; i < n; i++) {
-            pq.add(new Pair(matrix[i][0], i, 0));
-        }
-
-        // Remove smallest k-1 times
-        for (int i = 0; i < k - 1; i++) {
-
-            Pair p = pq.poll();
-
-            // Move to next element in same row
-            if (p.col + 1 < n) {
-                pq.add(new Pair(
-                    matrix[p.row][p.col + 1],
-                    p.row,
-                    p.col + 1
-                ));
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                n++;
+                if(n<=k){
+                    pq.add(matrix[i][j]);
+                }
+                else{
+                    if(pq.peek()>matrix[i][j]){
+                        pq.poll();
+                        pq.add(matrix[i][j]);
+                    }
+                }
             }
         }
-
-        return pq.poll().value;
+        return pq.poll();
     }
 }
