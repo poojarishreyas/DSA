700e2060da6b3581ebe527b39eb86f03b7279b0f
Shreyas Ananda Poojary <164512022+poojarishreyas@users.noreply.github.com>
2026-09-21 17:37:00 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (96.05%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,46 +1,25 @@
 class Solution {
-    int[][] dp;
-  public int maxProfit(int[] prices) {
-        dp=new int[prices.length+1][3];
+
+    public int maxProfit(int[] prices) {
+        int[] curr=new int[3];
+        int[] prev=new int[3];
 
        
         for(int i=prices.length-1;i>=0;i--){
             for(int j=1;j<3;j++){
                 if(j==2){
-                    dp[i][j]=Math.max(dp[i+1][j-1]-prices[i],dp[i+1][j]);
+                    curr[j]=Math.max(prev[j-1]-prices[i],prev[j]);
                 }
                 if(j==1){
-                    dp[i][j]=Math.max(dp[i+1][2]+prices[i],dp[i+1][j]);
+                   curr[j]=Math.max(prev[2]+prices[i],prev[j]);
 
                 }
             }
+            int[] temp=prev;
+            prev=curr;
+            curr=temp;
         }
-        return dp[0][2];
+        return prev[2];
     }
-    int helper(int[] prices, int i, int k) {
-
-    if (i == prices.length ) {
-        return 0;
-    }
-
-    if (dp[i][k] != -1) {
-        return dp[i][k];
-    }
-
-    if (k == 2) { // BUY
-
-        int c1 = helper(prices, i + 1, k - 1) - prices[i];
-        int c2 = helper(prices, i + 1, k);
-
-        return dp[i][k] = Math.max(c1, c2);
-
-    } else { // SELL
-
-        int c1 = helper(prices, i + 1, 2) + prices[i];
-        int c2 = helper(prices, i + 1, k);
-
-        return dp[i][k] = Math.max(c1, c2);
-    }
-}
-
+   
 }
