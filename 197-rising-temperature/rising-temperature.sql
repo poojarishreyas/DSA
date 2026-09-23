d989fcbb9fa9073d6c1191235b8b154a31d8d764
Shreyas Ananda Poojary <164512022+poojarishreyas@users.noreply.github.com>
2026-09-23 18:12:50 +0530

    Time: 475 ms (77.73%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -2,4 +2,4 @@
 select w1.id
 FROM weather w1
 INNER JOIN weather w2
-ON DATEDIFF(W1.recordDate,w2.recordDate)=1  AND w1.temperature>w2.temperature
+ON DATEDIFF(W1.recordDate,w2.recordDate)=1  where w1.temperature>w2.temperature
