b2c225075ff95d20789fbad6b5f7f6b6fbaf4a2f
Shreyas Ananda Poojary <164512022+poojarishreyas@users.noreply.github.com>
2026-09-24 15:57:51 +0530

    Time: 266 ms (92.90%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -3,7 +3,14 @@
 with t as(
     select t1.salary from Employee t1, Employee t2 where t1.salary<t2.salary
 )
-select max(salary ) as SecondHighestSalary from t*/
+select max(salary ) as SecondHighestSalary from t   
+
+
 select max(salary) as SecondHighestSalary
 From Employee 
-where salary<(select max(salary) from Employee)
+where salary<(select max(salary) from Employee)*/
+
+select(Select distinct(salary) 
+from Employee
+order by salary desc
+Limit 1 oFFset 1)as SecondHighestSalary
